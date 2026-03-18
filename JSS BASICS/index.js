42f59dd561cd7f4f63e73961e9b06cf7236f3dec
Rex-2-ui <rexraymond35@gmail.com>
2026-03-18 14:14:17 +0000

    update index.html with form elements and improve structure; add styles.css for button styling

--- JSS BASICS/index.js
+++ JSS BASICS/index.js
@@ -1,1 +1,81 @@
-window.alert("Ma Br3");
+// //window.alert("Ma Br3");
+// document.getElementById("yaa").textContent = "Avengers" ; // 0r getElementbyId
+// document.getElementById("konadu").textContent = "Grrrrr Pooowww";
+// let x = 5;
+// /*let x;
+//   x = 5  */
+//   let firstName ="BRUCE WANYE";
+//   console.log(x);
+//   console.log(firstName);
+//   console.log(`My name is ${firstName}`);
+//   // Boolean
+//   let tired = true;
+//   console.log(true);
+//   console.log('Linda Is Tired');
+//   let age, name, height;
+//   age = 18;
+//   name = Yaa ;
+//   height = 1.5;
+
+//   console.log(`My Name is ${name}`, `I am ${age} years old`, `I am ${height} centimeters` );
+
+
+
+
+// let firstName = "BATMAN"
+// console.log(`THE NAME ENTERED IS ${firstName}`)
+
+// let fullName = "YAA JACKSON";
+// let height = 165;
+// isHandsome = true;
+
+// document.getElementsByClassName("p1").textContent =
+// document.getElementsByClassName("p1").textContent =
+// document.getElementsByClassName("p1").textContent =
+
+// let x = 45;
+// x+=1;
+// x = x-12; x-=12;
+// x = x/5; x/5;
+// x = x*3;
+// x *= 3;
+// x %=20;
+
+// let x = 23 % 3 - 6/3;
+/* 2-6/3
+    2-2
+
+/*
+1. Parenthesis
+2. Exponents (**)
+3.Multiplication/Division/Modulus
+4. addition/substraction
+
+*/
+
+// let x = 10/2 ** (1 + 3)
+// let x =45 % 3** 4* 3/2 ;
+//
+
+/*
+1. use the window prompt
+2. use the text box
+
+*/
+
+// let username; window.prompt ("What is your Name?:")
+
+// console.log(username)
+// let username;
+// document.getElementsByClassName("btn").onclick = function (params) {
+
+//   username = document.getElementsByClassName("myName").value;
+// console.log(username)
+
+// document.getElementsByClassName()
+
+// }
+
+let quantity = window.prompt("How many iphones are you buying");
+let totalCost = Number(quantity) + 4;
+console.log(totalCost)
