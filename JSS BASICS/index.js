60109abfe63be520587fb11fc9f04e0955a2eeaa
Rex-2-ui <rexraymond35@gmail.com>
2026-06-08 10:04:12 +0000

    add login form and radius input; enhance button styles and layout

--- JSS BASICS/index.js
+++ JSS BASICS/index.js
@@ -19,9 +19,6 @@
 
 //   console.log(`My Name is ${name}`, `I am ${age} years old`, `I am ${height} centimeters` );
 
-
-
-
 // let firstName = "BATMAN"
 // console.log(`THE NAME ENTERED IS ${firstName}`)
 
@@ -76,6 +73,106 @@
 
 // }
 
-let quantity = window.prompt("How many iphones are you buying");
-let totalCost = Number(quantity) + 4;
-console.log(totalCost)
+// let quantity = window.prompt("How many iphones are you buying");
+// let totalCost = Number(quantity) + 4;
+// console.log(totalCost)
+// window.alert(`The total cost of your purchase is ${totalCost}`)
+
+// let firstName = "Yaa";
+// console.log(`firstName`);
+// console.log(firstName);
+// console.log(`My name is ${firstName}`)
+// const PI = 3.14159;
+// let radius;
+// let circumference;
+// document.getElementById("mySubmit").onclick = function () {
+// radius = document.getElementByID("myText").value;
+// radius = Number(radius);
+// circumference = 2 * PI * radius;
+// document.getElementById("myH3").textContent = circumference + "cm";
+// }
+
+// const decreaseBtn = document.getElementByID("decreaseBtn");
+// const increaseBtn = document.getElementByID("increaseBtn");
+// const resetBtn = document.getElementByID("resetBtn");
+// const countLabel = getElementById("countLabel");
+// let count = 0;
+// increaseBtn.onclick = function() {
+// count++;
+// countLabel.textContent = count;
+// }
+// decreaseBtn.onclick = function() {
+// count--;
+// countLabel.textContent = count;
+// }
+// resetBtn.onclick = function() {
+// count = 0;
+// countLabel.textContent = count;
+// }
+
+// const roll =document.getElementById("roll")
+// const mylabel =document.getElementById("mylabel")
+// const min = 1;
+// const max =6;
+// let randomNumber;
+// roll.onclick=function() {
+//     randomNumber=Math.floor((Math.round()*max)+min);
+//     mylabel.textContent=randomNumber;
+
+// }
+// console.log(typeof 5)
+
+// let age = 18;
+// if (age >= 18) {
+//     console.log("You are an adult");
+// } else {
+//     console.log("You are a minor");}
+
+//     let time = 14;
+//     if (time < 12) {
+//         console.log("Good Morning");
+//     }
+//     else {
+//         console.log("Good Afternoon");
+//     }
+
+//     let isEating = true;
+//     if (isEating) {
+//         console.log("Enjoy your meal");
+//     }
+//     else {        console.log("Are you hungry?");
+//     }
+//     let age = 25;
+//     let isMarriageable = true;
+
+//     if(age >= 18){
+//         console.log("You are old enough to marry");
+//         if(isMarriageable){
+//             console.log("Prepare to marry next month");
+//         }
+//         else{
+//             console.log("You are not ready to marry");
+//         }
+//     }
+//      else{
+//             console.log("You are not old enough to marry");
+//         }
+
+let myName = "   Linda  ";
+// Linda Amoako Banning;
+console.log(myName.charAt(3));
+console.log(myName.indexOf("a"));
+console.log(myName.lastIndexOf("a"));
+console.log(myName.length);
+console.log(myName.trim());
+
+//AND = &&
+//OR = ||
+//NOT = !
+let age = 20;
+let hasID = true;
+if (age >= 18 && hasID) {
+  console.log("You can enter the club");
+} else {
+  console.log("You cannot enter the club");
+}
