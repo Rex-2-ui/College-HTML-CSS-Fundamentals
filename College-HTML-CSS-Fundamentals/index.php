1e118dbacab7a2cb8ea1fdc61a1c722ca44aa87d
Rex-2-ui <rexraymond35@gmail.com>
2026-08-05 23:59:37 +0000

    #3

---
+++ College-HTML-CSS-Fundamentals/index.php
@@ -0,0 +1,14 @@
+<?php
+$age = 21;
+
+if ($age >= 18) {
+    echo "You are eligible to vote.";
+}
+else if ($age >= 0) {
+  echo "You arent Born";}
+else {
+    echo "You are not eligible to vote.";
+}
+
+
+?>
